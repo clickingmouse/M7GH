b833dadc1ed7fe97054cf3ef5684faf69a6ddd7a
clickingmouse <clickingmouse@users.noreply.github.com>
2018-10-10 10:10:00 +0800

    added SignedInLinks Component

--- m7gh/src/components/layout/Navbar.js
+++ m7gh/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
 const Navbar = () => {
   return (
     <nav class="nav-wrapper grey darken-3">
@@ -7,6 +8,7 @@ const Navbar = () => {
         <Link to="/" className="brand-logo">
           Majestic 7 Guesthouse
         </Link>
+        <SignedInLinks />
       </div>
     </nav>
   );
