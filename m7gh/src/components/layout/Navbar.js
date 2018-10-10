5bd2fda6f6e2041fc0d1541ec4908df064d6cd4d
clickingmouse <clickingmouse@users.noreply.github.com>
2018-10-10 10:14:19 +0800

    added SignedOutLinks Component

--- m7gh/src/components/layout/Navbar.js
+++ m7gh/src/components/layout/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
+
 const Navbar = () => {
   return (
     <nav class="nav-wrapper grey darken-3">
@@ -9,6 +11,7 @@ const Navbar = () => {
           Majestic 7 Guesthouse
         </Link>
         <SignedInLinks />
+        <SignedOutLinks />
       </div>
     </nav>
   );
