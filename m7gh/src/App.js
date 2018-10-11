99c36c8fe838a4417f045322208e72622cd43cd3
clickingmouse <clickingmouse@users.noreply.github.com>
2018-10-11 19:14:32 +0800

    tweaked navbar height & tied router Link

--- m7gh/src/App.js
+++ m7gh/src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/home/Home";
+import GettingHere from "./components/gettinghere/GettingHere";
+import Contact from "./components/contact/Contact";
+import GuidesGuide from "./components/guidesguide/GuidesGuide";
+import AroundUs from "./components/aroundus/AroundUs";
+import About from "./components/about/About";
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route path="/" component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/getting-here" component={GettingHere} />
+          <Route path="/around-us" component={AroundUs} />
+          <Route path="/guides-guide" component={GuidesGuide} />
+          <Route path="/contact" component={Contact} />
         </div>
       </BrowserRouter>
     );
---
+++ m7gh/src/components/about/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div class="home container">
+      <h4 class="center">About Us</h4>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere minima
+        fuga, esse fugiat eos nihil nostrum impedit asperiores eligendi
+        quisquam. Harum, impedit? Delectus laudantium dicta, accusantium a
+        quisquam iste distinctio?
+      </p>
+    </div>
+  );
+};
+
+export default About;
