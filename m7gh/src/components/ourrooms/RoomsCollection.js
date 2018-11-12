dca9f355e163a632d6867808cf5dd9ff2f5b1405
Kevin K <clickingmouse@hotmail.com>
2018-11-12 09:58:43 +0800

    set room pictures

--- m7gh/src/components/ourrooms/RoomsCollection.js
+++ m7gh/src/components/ourrooms/RoomsCollection.js
@@ -7,53 +7,158 @@ import tripleAImg from "../../images/tripleA.JPG";
 import budgetRoomImg1 from "../../images/budget/budget_room_1.JPG";
 import budgetRoomImg2 from "../../images/budget/budget_room_2.JPG";
 import budgetRoomImg3 from "../../images/budget/budget_room_3.JPG";
-
+import singleRoomImg1 from "../../images/single/single_room_6.JPG";
+import singleRoomImg2 from "../../images/single/single_room_7.JPG";
+import doubleRoomImg1 from "../../images/double/double_room_b.jpg";
+import doubleRoomImg2 from "../../images/double/double_room_b2.jpg";
+import twinRoomImg1 from "../../images/twin/twin_room_b.jpg";
+import twinRoomImg2 from "../../images/twin/twin_room_b2.JPG";
+import tripleRoomImg1 from "../../images/triple/triple_room_4.jpg";
+import tripleRoomImg2 from "../../images/triple/triple_room_8.JPG";
 
 const RoomsCollection = () => {
   return (
     <div>
-  <ul className="collection">
-    <li className="collection-item ">
-    <div className="row">
-    <div className="col s12  l4">
-              <img src={budgetRoomImg1} alt="SingleRoomIMG" className="responsive-img materialboxed" />
+      <ul className="collection">
+        <li className="collection-item ">
+          <div className="row">
+            <div className="col s16  l4">
+              <img
+                src={budgetRoomImg1}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
             </div>
-            <div className="col s12 m4 l4">
-              <img src={budgetRoomImg2} alt="SingleRoomIMG" className="responsive-img materialboxed" />
+
+            <div className="col s16 m4 l4">
+              <img
+                src={budgetRoomImg2}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
             </div>
- 
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      </div>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-    <li className="collection-item avatar">
-      <i className="material-icons circle">folder</i>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i classNamee="material-icons">grade</i></a>
-    </li>
-    <li className="collection-item avatar">
-      <i className="material-icons circle green">insert_chart</i>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-    <li className="collection-item avatar">
-      <i className="material-icons circle red">play_arrow</i>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-  </ul>
+
+            <span className="title">
+              <bold>Budget Single</bold>
+            </span>
+            <p>
+              First Line <br />
+              Second Line
+            </p>
+          </div>
+        </li>
+
+        <li className="collection-item ">
+          <div className="row">
+            <div className="col s16  l4">
+              <img
+                src={singleRoomImg1}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <div className="col s16 m4 l4">
+              <img
+                src={singleRoomImg2}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <span className="title">
+              <bold>Single Room</bold>
+            </span>
+            <p>
+              First Line <br />
+              Second Line
+            </p>
+          </div>
+        </li>
+
+        <li className="collection-item ">
+          <div className="row">
+            <div className="col s16  l4">
+              <img
+                src={doubleRoomImg1}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <div className="col s16 m4 l4">
+              <img
+                src={doubleRoomImg2}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <span className="title">
+              <bold>Double Room</bold>
+            </span>
+            <p>
+              First Line <br />
+              Second Line
+            </p>
+          </div>
+        </li>
+
+        <li className="collection-item ">
+          <div className="row">
+            <div className="col s16  l4">
+              <img
+                src={twinRoomImg1}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <div className="col s16 m4 l4">
+              <img
+                src={twinRoomImg2}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <span className="title">
+              <bold>Twin</bold>
+            </span>
+            <p>
+              First Line <br />
+              Second Line
+            </p>
+          </div>
+        </li>
+        <li className="collection-item ">
+          <div className="row">
+            <div className="col s16  l4">
+              <img
+                src={tripleRoomImg1}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <div className="col s16 m4 l4">
+              <img
+                src={tripleRoomImg2}
+                alt="SingleRoomIMG"
+                className="responsive-img materialboxed"
+              />
+            </div>
+
+            <span className="title">
+              <bold>Triple</bold>
+            </span>
+            <p>
+              First Line <br />
+              Second Line
+            </p>
+          </div>
+        </li>
+      </ul>
       {/****************************************** */}
       <ul className="collection">
         <li className="collection-header">
@@ -62,7 +167,11 @@ const RoomsCollection = () => {
         <li className="collection-item ">
           <div className="row">
             <div className="col s12 l8">
-              <img src={budgetImg} alt="SingleRoomIMG" className="responsive-img" />
+              <img
+                src={budgetImg}
+                alt="SingleRoomIMG"
+                className="responsive-img"
+              />
             </div>
             <span className="title">Budget Room</span>
             <p>
@@ -78,7 +187,11 @@ const RoomsCollection = () => {
           {" "}
           <div className="row">
             <div className="col s12 l8">
-              <img src={singleImg} alt="SingleRoomIMG" className="responsive-img" />
+              <img
+                src={singleImg}
+                alt="SingleRoomIMG"
+                className="responsive-img"
+              />
             </div>
             <span className="title">Title</span>
             <p>
