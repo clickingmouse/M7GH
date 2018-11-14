ed2161a6421b5f94dc337054dc8482cff39f0323
Kevin K <clickingmouse@hotmail.com>
2018-11-14 11:07:11 +0800

    basic guides & auth component

--- m7gh/src/App.js
+++ m7gh/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/home/Home";
 import GettingHere from "./components/gettinghere/GettingHere";
 import Contact from "./components/contact/Contact";
 import GuidesGuide from "./components/guidesguide/GuidesGuide";
 import Guides from "./components/guides/Guides";
-
+import GuideDetails from "./components/guides/GuideDetails";
 import AroundUs from "./components/aroundus/AroundUs";
 import About from "./components/about/About";
 import Footer from "./components/layout/Footer";
 import OurRooms from "./components/ourrooms/OurRooms";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 class App extends Component {
   render() {
@@ -18,14 +20,19 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route path="/home" component={Home} />
-          <Route path="/rooms" component={OurRooms} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/home" component={Home} />
+            <Route path="/rooms" component={OurRooms} />
 
-          <Route path="/about" component={About} />
-          <Route path="/getting-here" component={GettingHere} />
-          <Route path="/around-us" component={AroundUs} />
-          <Route path="/guides" component={Guides} />
-          <Route path="/contact" component={Contact} />
+            <Route path="/about" component={About} />
+            <Route path="/getting-here" component={GettingHere} />
+            <Route path="/around-us" component={AroundUs} />
+            <Route exact path="/guides" component={Guides} />
+            <Route path="/guides/:id" component={GuideDetails} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
           <Footer />
         </div>
       </BrowserRouter>
---
+++ m7gh/src/components/guides/GuidesList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GuidesSummary from "./GuidesSummary";
+
+const GuidesList = () => {
+  return (
+    <div className="guide-list section">
+      <GuidesSummary />
+      <GuidesSummary />
+      <GuidesSummary />
+      <GuidesSummary />
+    </div>
+  );
+};
+
+export default GuidesList;
