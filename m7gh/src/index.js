8162dcbf4137b1ea753e33d79a5acad3fa759816
Kevin K <clickingmouse@hotmail.com>
2018-11-14 17:42:15 +0800

    added redux basic boilers

--- m7gh/src/index.js
+++ m7gh/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
---
+++ m7gh/src/store/reducers/guideReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  //guides property state
+ guides :[
+  {id: '1', title:'airport', content:'a21'}
+  {id:'2', title:'chinese food', content:'dimsum'},
+{id:'3', title:'view', content:'peak'}
+] 
+
+};
+
+const guideReducer = (state = initialState, action) => {
+  return state;
+};
+export default guideReducer;
