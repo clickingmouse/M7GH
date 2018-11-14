dfabd3544bd4e35c191ce5076d3a9e5e2a37755d
Kevin K <clickingmouse@hotmail.com>
2018-11-14 18:16:19 +0800

    tied in basic redux store

--- m7gh/src/components/guides/GuidesSummary.js
+++ m7gh/src/components/guides/GuidesSummary.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const GuidesSummary = () => {
+const GuidesSummary = ({ guide }) => {
   return (
     <div className="card z-depth-0 guide-list-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Guide's Title</span>
+        <span className="card-title">{guide.title}</span>
         <p>Posted by M7</p>
         <p className="grey-text">3rd September 2AM</p>
       </div>
--- m7gh/src/store/reducers/guideReducer.js
+++ m7gh/src/store/reducers/guideReducer.js
@@ -1,11 +1,10 @@
 const initialState = {
   //guides property state
- guides :[
-  {id: '1', title:'airport', content:'a21'}
-  {id:'2', title:'chinese food', content:'dimsum'},
-{id:'3', title:'view', content:'peak'}
-] 
-
+  guides: [
+    { id: "1", title: "airport", content: "a21" },
+    { id: "2", title: "chinese food", content: "dimsum" },
+    { id: "3", title: "view", content: "peak" }
+  ]
 };
 
 const guideReducer = (state = initialState, action) => {
